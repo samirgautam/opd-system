1cd999fc089c3ed4840917e8242f39dbd179bbb3
Arjun Regmi <arjunregmi3344@gmail.com>
2022-12-11 13:37:04 +0545

    completed booking controller crud with validation

--- resources/views/booking/bookingform.blade.php
+++ resources/views/booking/bookingform.blade.php
@@ -3,70 +3,50 @@
   <div class="container">
 
     <div class="card card-body shadow">
-      @if($errors->any()) {{ $errors }} @endif
+      @if($errors->any())
+        @foreach ($errors->all() as $err )
+          <div class="alert alert-danger">
+            {{ $err }}
+          </div>
+        @endforeach
+       @endif
       <h5 class="card-text mb-3">Booking Form</h5>
       <form action="{{ route('savebooking') }}" method="post">
         @csrf
         <div class="form-group">
           <label for="name">Name</label>
-          <input type="text" name="name" id="name" class="form-control">
-          <div class="form-text">
-              @if ($errors->has('name'))
-                {{ $errors->first() }}
-              @endif
-          </div>
+          <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
         </div>
         <div class="form-group">
           <label for="address">Address</label>
-          <input type="text" name="address" id="address" class="form-control">
-          <div class="form-text">
-            @if ($errors->has('address'))
-              {{ $errors }}
-            @endif
-        </div>
+          <input type="text" name="address" id="address" class="form-control" value="{{ old('adddress') }}">
+          
         </div>
         <div class="form-group">
           <label for="age">Age</label>
-          <input type="number" name="age" id="age" class="form-control">
-          <div class="form-text">
-            @if ($errors->has('name'))
-              {{ $errors }}
-            @endif
-        </div>
+          <input type="number" name="age" id="age" class="form-control" value="{{ old('name') }}">
+          
         </div>
         <div class="form-group">
           <label for="sex">Gender</label>
           <select class="form-select" name="sex">
-            <option selected>.....................</option>
-            <option value="male">Male</option>
+            <option value="male" selected >Male</option>
             <option value="female">Female</option>
           </select>
-          <div class="form-text">
-            @if ($errors->has('name'))
-              {{ $errors }}
-            @endif
-        </div>
+          
         </div>
         <div class="form-group">
           <label for="mobile_num">Mobile Number</label>
-          <input type="text" name="mobile_num" id="mobile_num" class="form-control">
-          <div class="form-text">
-            @if ($errors->has('name'))
-              {{ $errors }}
-            @endif
-        </div>
+          <input type="text" name="mobile_num" id="mobile_num" class="form-control" value="{{ old('name') }}">
+          
         </div>
         <div class="form-group mt-3">
           <label for="is_paid">Payment Status</label>&nbsp;&nbsp;&nbsp;
-            <input class="form-check-input" type="checkbox" value="1" id="is_paid" name="is_paid">
+            <input class="form-check-input" type="checkbox" value="1" id="is_paid" name="is_paid" value="{{ old('name') }}">
             <label class="form-check-label" for="is_paid">
               Paid
             </label>
-            <div class="form-text">
-              @if ($errors->has('name'))
-                {{ $errors }}
-              @endif
-          </div>
+            
         </div>
         <div class="form-group mt-3">
           <button type="submit" class="btn btn-primary">Book Now</button>
